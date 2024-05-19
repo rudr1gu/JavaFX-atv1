52b9dc8322a4ba3238c4728f95283710005e3e64
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2024-05-19 15:40:06 -0300

    firt commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## Atividade 1 de JavaFX
+
+<img src="./img/scene1.png">
+<img src="./img/scene2.png">
---
+++ src/aplicacaofxml/FXMLDocumentController.java
@@ -0,0 +1,27 @@
+package aplicacaofxml;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+    private Label lblMensagem;
+    private Button btnClick;
+    
+    @FXML
+    private void clicouBotao(ActionEvent event) {
+        lblMensagem.setText("Olá Rudr1gu!");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
